a8a4bd3651b7b0d99a4e51d0fbc9fe3f8539e52e
GregjQU <gjeanbaptiste@quinnipiac.edu>
2023-10-02 00:13:41 -0400

    before implementing attack

--- src/Level/Player.java
+++ src/Level/Player.java
@@ -43,6 +43,7 @@ public abstract class Player extends GameObject {
     protected Key MOVE_LEFT_KEY = Key.A;
     protected Key MOVE_RIGHT_KEY = Key.D;
     protected Key CROUCH_KEY = Key.S;
+    protected Key SWING_KEY = Key.Q;// testing button for swing annimation
 
     // flags
     protected boolean isInvincible = false; // if true, player cannot be hurt by enemies (good for testing)
