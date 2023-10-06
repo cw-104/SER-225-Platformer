ecf1e593c1f4560ac16b94f65a1c6c3ec6779b35
Jason Jackson <j.a.jackson1029@gmail.com>
2023-10-06 16:14:05 -0400

    Guard Sprite Sheet Testing

--- src/Tilesets/LabTileset.java
+++ src/Tilesets/LabTileset.java
@@ -14,31 +14,42 @@ import java.util.ArrayList;
 // This class represents a "common" tileset of standard tiles defined in the CommonTileset.png file
 public class LabTileset extends Tileset {
 
-    public LabTileset() {
-        super(ImageLoader.load("LabTileset.png"), 50, 50, 3);
-    }
+        public LabTileset() {
+                super(ImageLoader.load("LabTileset.png"), 50, 50, 3);
+        }
 
-    @Override
-    public ArrayList<MapTileBuilder> defineTiles() {
-        ArrayList<MapTileBuilder> mapTiles = new ArrayList<>();
+        @Override
+        public ArrayList<MapTileBuilder> defineTiles() {
+                ArrayList<MapTileBuilder> mapTiles = new ArrayList<>();
 
-        // grass
-        Frame grassFrame = new FrameBuilder(getSubImage(0, 0))
-                .withScale(tileScale)
-                .build();
+                /*
+                 * Frame Preset
+                 * 
+                 * Frame frameName = new FrameBuilder(getSubImage(0,0))
+                 * .withScale(tileScale)
+                 * .build();
+                 * MapTileBuilder tileName = new MapTileBuilder(frameName)
+                 * .withTileType(TileType.TYPE);
+                 * mapTiles.add(tileName)
+                 */
 
-        MapTileBuilder grassTile = new MapTileBuilder(grassFrame)
-                .withTileType(TileType.NOT_PASSABLE);
+                // floor tile 1
+                Frame floor1Frame = new FrameBuilder(getSubImage(0, 0))
+                                .withScale(tileScale)
+                                .build();
 
-        mapTiles.add(grassTile);
+                MapTileBuilder floor1Tile = new MapTileBuilder(floor1Frame)
+                                .withTileType(TileType.NOT_PASSABLE);
 
-        Frame blueFrame = new FrameBuilder(getSubImage(0, 1))
-                .withScale(tileScale)
-                .build();
-        MapTileBuilder blueTile = new MapTileBuilder(blueFrame)
-                .withTileType(TileType.NOT_PASSABLE);
-        mapTiles.add(blueTile);
+                mapTiles.add(floor1Tile);
 
-        return mapTiles;
-    }
+                Frame blueFrame = new FrameBuilder(getSubImage(0, 1))
+                                .withScale(tileScale)
+                                .build();
+                MapTileBuilder blueTile = new MapTileBuilder(blueFrame)
+                                .withTileType(TileType.NOT_PASSABLE);
+                mapTiles.add(blueTile);
+
+                return mapTiles;
+        }
 }
