239aefc7ca2103da6d9b50bf0c3e45e15375827a
GregjQU <gjeanbaptiste@qu.edu>
2023-10-19 17:25:47 -0400

    attempting to make speedpower up, created class

---
+++ src/Level/SpeedPowerUp.java
@@ -0,0 +1,21 @@
+package Level;
+// class is going to be used to have max speed up once the power up is gained.
+//nts: pic icon that could be used for the visual representation of the  power-up
+//use coin class as possible reference 
+import GameObject.GameObject;
+
+public class SpeedPowerUp  extends GameObject{
+
+    public SpeedPowerUp(float x, float y) {
+        super(x, y);
+        //TODO Auto-generated constructor stub
+    }
+    
+
+
+
+
+
+
+
+}
